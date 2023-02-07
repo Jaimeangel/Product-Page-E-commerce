9a9e6f3c296ab191407772620a1b0b70965b56ca
Jaime A <jaimeangel1097@gmail.com>
2023-02-07 16:46:53 -0500

    creando modal para slider de imagenes/completando el prototip con todos lo smetodos

--- slider_img.js
+++ slider_img.js
@@ -29,8 +29,7 @@ class SliderImages{
             }
         ];
     }
-
-    ChangeImageByArrow(type){
+    ChangeImageByArrow(type,id){
         if(type==='plus'){
             if(this.currentImage===4){
                 return
@@ -42,14 +41,13 @@ class SliderImages{
             }
             this.currentImage=this.currentImage-1
         }
-        this.ChangeImage()
+        this.ChangeImage(id)
     }
-    ChangeImageByImage(number){
+    ChangeImageByImage(number,id){
         this.currentImage=number;
-        this.ChangeImage()
+        this.ChangeImage(id)
     }
-
-    loadContent(){
+    loadContent(nodoWrap){
         const wrap=document.createElement('div');
         wrap.classList.add('slideImagees')
 
@@ -57,26 +55,113 @@ class SliderImages{
         const slide=document.createElement('slide')
         slide.classList.add('slide')
 
+        /* prev btn */
         const prevBtn=document.createElement('button')
-        prevBtn.onclick=()=>this.ChangeImageByArrow('minus')
+        prevBtn.onclick=()=>this.ChangeImageByArrow('minus','mainImg')
 
         prevBtn.classList.add('btnSlide')
         prevBtn.classList.add('prev')
         const prevImg=document.createElement('img')
         prevImg.setAttribute('src','./images/icon-previous.svg')
         prevBtn.appendChild(prevImg)
-
+        
+        /* Image Slide */
         const mainCtn=document.createElement('div')
         mainCtn.classList.add('ctnImg')
         const mainImg=document.createElement('img')
         mainImg.setAttribute('id','mainImg')
         mainCtn.appendChild(mainImg)
 
+        /* next btn */
         const nextBtn=document.createElement('button')
-        nextBtn.onclick=()=>this.ChangeImageByArrow('plus')
+        nextBtn.onclick=()=>this.ChangeImageByArrow('plus','mainImg')
 
         nextBtn.classList.add('btnSlide')
         nextBtn.classList.add('next')
+        const nextImg=document.createElement('img')
+        nextImg.setAttribute('src','./images/icon-next.svg')
+        nextBtn.appendChild(nextImg)
+
+        /* Button Max */
+        const btnMax=document.createElement('button')
+        btnMax.onclick=()=>this.Modal()
+
+        btnMax.classList.add('btnMax')
+        const icon=document.createElement('i')
+        icon.classList.add('fa-solid')
+        icon.classList.add('fa-maximize')
+        btnMax.append(icon)
+
+        slide.append(prevBtn,mainCtn,nextBtn,btnMax)
+
+        /* list little images */
+        const listImg=document.createElement('div')
+        listImg.classList.add('listSlide')
+        this.images.forEach((img,index)=>{
+            const id=index+1
+            const image=document.createElement('img')
+            image.onclick=()=>this.ChangeImageByImage(id,'mainImg')
+            image.setAttribute('src',`${img.img_thb}`)
+            listImg.append(image)
+        })
+
+        wrap.append(slide,listImg)
+        nodoWrap.appendChild(wrap)
+        this.ChangeImage('mainImg')
+    }
+    ChangeImage(id){
+        const ctnImg=document.getElementById(`${id}`);
+        const imgToSelect=this.images.find(img=>img.id===this.currentImage);
+        ctnImg.setAttribute('src',`${imgToSelect.img}`)
+    }
+    CloseModal(nodo){
+        nodo.close()
+        nodo.remove()
+    }
+    Modal(){
+        const wrapModal=document.createElement('div')
+        wrapModal.classList.add('wrapModal')
+
+        const modal=document.createElement('dialog')
+        const btnClose=document.createElement('button')
+        btnClose.classList.add('btnClose')
+        btnClose.onclick=()=>this.CloseModal(modal)
+        const iconClose=document.createElement('i')
+        iconClose.classList.add('fa-solid')
+        iconClose.classList.add('fa-xmark')
+        btnClose.append(iconClose)
+
+        const wrap=document.createElement('div');
+        wrap.classList.add('slideImagees')
+
+        /* Slide */
+        const slide=document.createElement('slide')
+        slide.classList.add('slide')
+        slide.classList.add('slideModal')
+
+        /* prev btn */
+        const prevBtn=document.createElement('button')
+        prevBtn.onclick=()=>this.ChangeImageByArrow('minus','mainImgModal')
+
+        prevBtn.classList.add('btnSlideModal')
+        prevBtn.classList.add('prevModal')
+        const prevImg=document.createElement('img')
+        prevImg.setAttribute('src','./images/icon-previous.svg')
+        prevBtn.appendChild(prevImg)
+        
+        /* Image Slide */
+        const mainCtn=document.createElement('div')
+        mainCtn.classList.add('ctnImg')
+        const mainImg=document.createElement('img')
+        mainImg.setAttribute('id','mainImgModal')
+        mainCtn.appendChild(mainImg)
+
+        /* next btn */
+        const nextBtn=document.createElement('button')
+        nextBtn.onclick=()=>this.ChangeImageByArrow('plus','mainImgModal')
+
+        nextBtn.classList.add('btnSlideModal')
+        nextBtn.classList.add('nextModal')
         const nextImg=document.createElement('img')
         nextImg.setAttribute('src','./images/icon-next.svg')
         nextBtn.appendChild(nextImg)
@@ -89,24 +174,24 @@ class SliderImages{
         this.images.forEach((img,index)=>{
             const id=index+1
             const image=document.createElement('img')
-            image.onclick=()=>this.ChangeImageByImage(id)
+            image.onclick=()=>this.ChangeImageByImage(id,'mainImgModal')
             image.setAttribute('src',`${img.img_thb}`)
             listImg.append(image)
         })
 
         wrap.append(slide,listImg)
-        this.nodo.appendChild(wrap)
-        this.ChangeImage()
-    }
 
-    ChangeImage(){
-        const ctnImg=document.getElementById('mainImg');
-        const imgToSelect=this.images.find(img=>img.id===this.currentImage);
-        ctnImg.setAttribute('src',`${imgToSelect.img}`)
-    }
+        wrapModal.append(btnClose,wrap)
+        modal.append(wrapModal)
 
-    
+        window.document.body.appendChild(modal)
+        modal.showModal()
+        this.ChangeImage('mainImgModal')
+    }
+    Start(){
+        this.loadContent(this.nodo)
+    }
 }
 
 const Slider=new SliderImages({nodo:content})
-Slider.loadContent()
+Slider.Start()
